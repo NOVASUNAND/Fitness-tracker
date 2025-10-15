8d2a939ef4b78eac0f23ae52044d943dd0abbdb2
Sunand <sunandparvathaneni@gmail.com>
2025-10-15 19:20:29 +0530

    fourth commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://fitness-tracker-sun.netlify.app' }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
