9239e8a38a874d12b2181489c523c588867f648e
Sunand <sunandparvathaneni@gmail.com>
2025-11-14 19:29:26 +0530

    Add Vercel URL to CORS whitelist

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://fitness-tracker-sun.netlify.app" 
+  //"https://fitness-tracker-sun.netlify.app" 
+  "https://your-new-vercel-url.vercel.app"
 ];
 
 // We define the cors options
